3ca2565908d0331f5fd7029b96f9ab8697af15c1
trichoip <107303342+trichoip@users.noreply.github.com>
2022-11-08 12:41:47 +0700

    update feature

--- src/main/java/com/etransportation/payload/dto/FeatureDTO.java
+++ src/main/java/com/etransportation/payload/dto/FeatureDTO.java
@@ -1,11 +1,15 @@
 package com.etransportation.payload.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
 public class FeatureDTO {
 
     private Long id;
+    @JsonIgnore
     private String name;
+    @JsonIgnore
     private String icon;
 }
