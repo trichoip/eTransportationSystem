51c08efb6f152d195b2475b7e0038c8784c88b9e
trichoip <107303342+trichoip@users.noreply.github.com>
2023-07-21 01:34:44 +0700

    fix mysql script

--- fulldata-mysql.sql
+++ fulldata-mysql.sql
@@ -1,5 +1,5 @@
---CREATE DATABASE  IF NOT EXISTS `etransportationsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
---USE `etransportationsystem`;
+-- CREATE DATABASE  IF NOT EXISTS `etransportationsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- USE `etransportationsystem`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: etransportationsystem
